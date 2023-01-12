a8370ab3997a06956bd95b11181617eb468adce1
Reza <rezarafi64@gmail.com>
2023-01-12 12:36:44 -0700

    add else

--- friends.js
+++ friends.js
@@ -2,7 +2,11 @@ var arrays = ["Jane", "Rain", "Jack", "Peter", "Sara"]
 arrays.pop()
 console.log (arrays)
 
-var besti = ["anna", "mammad"]
+var besti = "anna"
 for(i=0; i<besti.length; i++) {if (besti[i].length<5){
     console.log(besti[i])
 }}
+
+if (besti === "ann") {
+    console.log("good friend")
+} else {console.log("such and such friend")}
