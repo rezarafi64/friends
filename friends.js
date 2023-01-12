d1604818358030480cab1d6716c5f44293d28307
Reza <rezarafi64@gmail.com>
2023-01-12 12:30:36 -0700

    add if

--- friends.js
+++ friends.js
@@ -1,3 +1,8 @@
 var arrays = ["Jane", "Rain", "Jack", "Peter", "Sara"]
 arrays.pop()
 console.log (arrays)
+
+var besti = ["anna", "mammad"]
+for(i=0; i<besti.length; i++) {if (besti[i].length<5){
+    console.log(besti[i])
+}}
